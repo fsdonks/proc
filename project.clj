8b40bafe47418c9a91a8c44ffebe65dde8f634bf
Pavlak <michael.pavlak@caa.army.smil.mil>
2017-07-10 13:32:48 -0400

    updates to do charts and other functions
    [added chart saving functions for bulk export ops.]

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [spork "0.1.9.9-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [spork "0.2.0.2-SNAPSHOT"]
                  [incanter "1.5.6"]
                  [iota "1.1.2"]]
   :repl-options {:init (do (println "Loading post processor")
--- src/proc/example.clj
+++ src/proc/example.clj
@@ -30,6 +30,56 @@ in your interests?"
 ;Can do trac-like charts by demand group by doing (binding [proc.stacked/*by-demandgroup?* true] 
 ;                                                        (do-charts-from root :interests blah))
 
+(def marathons (atom {})) ;;Stores history of all marathon runs where key is title and val is the chart objs vector
+(def chart-info {:title 0 :dwell 1 :fill 2}) ;; used to pull specific peices out of chart vector
+
+(defn all-chart-types [key & col] ;;given a key and vector of components, returns the compentent specified by key
+  (let [c (if col (first col) (vals @marathons))] ;;default collection in global history of marathon runs
+    (map #(nth % (get chart-info key)) c)))
+
+(defn get-chart-title [chart] ;; returns the title of the chart in form [run]-[interest]
+  (let [htmlstr (.getText (first (all-chart-types :title [chart])))
+        s clojure.string/split
+        ttl (fn [key] (cond (= :run key) (first (s (last (s htmlstr #"Run: ")) #"<br>"))
+                            (= :interest key) (first (s (last (s htmlstr #"Interest: ")) #"</b>"))))]
+    (str (ttl :run) "-" (ttl :interest))))
+
+(defn sync-chart-scales [coll axis] ;;syncs the axies of the given collection of charts
+  (if (= axis :all)
+    (do (proc.util/sync-scales coll :axis :x-axis)
+        (proc.util/sync-scales coll :axis :y-axis))
+    (proc.util/sync-scales coll :axis axis)) nil)
+
+(defn show-chart [chart] ;;makes the chart visible given the collection on components
+  (let [frame (show-stack chart)
+        title (get-chart-title chart)]
+    (.setTitle frame title)))
+    ;;(.setIconImage frame (spork.graphics2d.image/read-buffered-image "C:\\Users\\michael.pavlak\\Desktop\\icon.JPG"))))
+
+    
+
+;; saves the jfree chart given the chart vector of chart objs, the title of the chart, and key (:dwell or :fill)
+(defn save-jfree-chart [chart title key & {:keys [width height full-title] :or {width 700 height 420 full-title true}}] 
+  (let [t (fn [ntitle] (.setTitle (first (all-chart-types key [chart])) ntitle))] 
+    (when full-title ;; When full-title, change the title of the dwell or fill chart to include run + intereset
+      (let [ftitle (get-chart-title chart)]
+        (when (= key (keyword "fill")) (t (str ftitle " - Fill")))
+        (when (= key (keyword "dwell")) (t (str ftitle " - Dwell")))))
+    (org.jfree.chart.ChartUtilities/saveChartAsPNG
+     (java.io.File. title) (first (all-chart-types key [chart])) width height)
+    (when (= key (keyword "fill")) (t "Fill")) ;; Change title of chart back to short title 
+    (when (= key (keyword "dwell")) (t "Dwell"))))
+   
+(defn get-theme [font-name] ;; Changes font to larger size of default ChartFactory theme
+  (let [theme (org.jfree.chart.ChartFactory/getChartTheme)] 
+    (doto theme
+      (.setExtraLargeFont (java.awt.Font. font-name (java.awt.Font/BOLD) 26))
+      (.setLargeFont (java.awt.Font. font-name (java.awt.Font/BOLD) 22))
+      (.setRegularFont (java.awt.Font. font-name (java.awt.Font/PLAIN) 14))
+      (.setSmallFont (java.awt.Font. font-name (java.awt.Font/PLAIN) 12))
+      )theme))
+
+
 ;Do-charts-from creates stacked dwell and fill charts for each interest using AUDIT_deployments.txt and
 ;the fills files produced from proc.fillsfils/run-sample!.
 ;You need to call run-sample! in order to make a folder for fills files within the Marathon run folder before calling do-charts-from
@@ -41,40 +91,107 @@ Call with :phases set to a sequence of phases defined in the run in order to see
 a null pointer exception if one of your phases doesn't exist in the run.
 Call with :fillbnds {:fxlow val0 :fxhigh val1 :fylow val2 :fyhigh val3} and/or 
 :dwellbnds {:dxlow val4 :dxhigh val5 :dylow val6 :dyhigh val7} to set the bounds for axes."
-  [root & {:keys [subs phases interests subints group-key syncys phase-lines fillbnds dwellbnds] :or {subs false phases [nil] syncys false
-                                                                interests ints/defaults group-key :DemandType
-                                                                phase-lines true
-                                                                fillbnds {:fxlow 0 :fylow 0}
-                                                                dwellbnds {:dxlow 0}}}]
+  [root &
+   {:keys [subs phases interests subints group-key syncys phase-lines fillbnds dwellbnds vis save-fill save-dwell]
+    :or {subs false phases [nil] syncys false interests ints/defaults group-key :DemandType phase-lines true vis true
+         save-fill false save-dwell false fillbnds {:fxlow 0 :fylow 0} dwellbnds {:dxlow 0}}}]
+  (org.jfree.chart.ChartFactory/setChartTheme (get-theme "Calibri"))
   (let [ints (if subints subints (keys interests))
-        charts (only-by-interest  ints interests
-                       (remove nil? (for [int ints ;remove nil printlns
-                                                              phase phases]
-                                                          (binding [dep-group-key group-key] 
-                                                            (dwell-over-fill root (get interests int) subs phase)))))
-        dwells (->> (map (fn [[pane dwell fill]] dwell) charts)
-                 (remove (fn [chart] (nil? (.getDataset (.getPlot chart)))))) ;had to remove no deployment data charts
-        fills (map (fn [[pane dwell fill]] fill) charts)
+        charts (only-by-interest ints interests
+                                 (remove nil? (for [int ints phase phases] ;remove nil printlns
+                                                (binding [dep-group-key group-key] 
+                                                  (dwell-over-fill root (get interests int) subs phase)))))
+        dwells (remove #(nil? (.getDataset (.getPlot %))) (all-chart-types :dwell charts))
+        fills  (all-chart-types :fill charts)
+        phstarts (phase-starts root)
+
         {:keys [fxlow fxhigh fylow fyhigh]} fillbnds
         {:keys [dxlow dxhigh dylow dyhigh]} dwellbnds
-        _ (doseq [chart dwells] (do (proc.util/set-bounds chart :x-axis :lower dxlow :upper dxhigh)
-                                  (proc.util/set-bounds chart :y-axis :lower dylow :upper dyhigh)))
-        _ (doseq [chart fills] (do (proc.util/set-bounds chart :x-axis :lower fxlow :upper fxhigh)
-                                  (proc.util/set-bounds chart :y-axis :lower fylow :upper fyhigh)))
-        _ (proc.util/sync-scales (concat dwells fills) :axis :x-axis)   ;added for taa. want all charts same x
-        _ (when syncys (do  ;it might be useful if our y-axes all match, too
-                       (proc.util/sync-scales dwells :axis :y-axis)
-                       (proc.util/sync-scales fills :axis :y-axis)))
-        _  (doseq [dwell dwells] (add-trend-lines! dwell :bnds (:x (xy-bounds dwell)))) ;need to add trend lines after syncing charts
-        phstarts (phase-starts root)
-        _ (when phase-lines (doseq [fill fills] (add-phase-lines phstarts 
-                                                                 (.getLowerBound (:y-axis (util/state fill))) 
-                                                                 (.getUpperBound (:y-axis (util/state fill))) 
-                                                               fill)))
-       ; _ (doseq [fill fills] (Image.IO/write fill "jpg" "v:/"))
-        ]
-    (doseq [chart charts] (show-stack chart))))
-       
+        chart-bounds (fn [coll]
+                       (doseq [chart coll :let [sb (fn [axis l h] (proc.util/set-bounds chart axis :lower l :upper h))]]
+                         (sb :x-axis dxlow dxhigh) (sb :y-axis dylow dyhigh)))]
+    
+    (doseq [chart charts] (swap! marathons assoc (get-chart-title chart) chart)) ;;adds new charts to global history
+    
+    (doseq [k [:dwell :fill]] ;; sets chart boundaries and syncs scales  
+      (chart-bounds (all-chart-types k))
+      (let [a (if syncys :all :x-axis)]
+        (sync-chart-scales (all-chart-types k) a)))
+    (sync-chart-scales (concat dwells fills) :x-axis)
+    
+    (doseq [dwell dwells] (add-trend-lines! dwell :bnds (:x (xy-bounds dwell)))) ;;adds trend lines
+    (when phase-lines ;; adds phase-lines
+      (doseq [fill (all-chart-types :fill)
+              :let [b (:y-axis (util/state fill)) l (fn [] (.getLowerBound b)) u (fn [] (.getUpperBound b))]]
+        (add-phase-lines phstarts (l) (u) fill)))
+
+    ;;makes charts visable, default true
+    (when vis (doseq [chart charts] (show-chart chart)))
+
+    (when (or save-dwell save-fill) ;;saves dwell/fill charts 
+      (doseq [chart charts :let [title (get-chart-title chart)]]
+        (when save-fill (save-jfree-chart chart (str root title "-fill.png") :fill))
+        (when save-dwell (save-jfree-chart chart (str root title "-dwell.png") :dwell))))))
+
+(defn do-multiple-charts-from ;; calls do-charts-from for each root in roots using the same options for each
+ [roots &
+  {:keys [subs phases interests subints group-key syncys phase-lines fillbnds dwellbnds vis save-fill save-dwell]
+   :or {subs false phases [nil] syncys false interests ints/defaults group-key :DemandType phase-lines true vis true
+        save-fill false save-dwell false fillbnds {:fxlow 0 :fylow 0} dwellbnds {:dxlow 0}}}]
+  (doseq [root roots]
+   (do-charts-from root
+                   :subs subs :phases phases :interests interests :subints subints :group-key group-key
+                   :syncys syncys :phase-lines phase-lines :fillbnds fillbnds :dwellbnds dwellbnds
+                   :vis vis :save-fill save-fill :save-dwell save-dwell)))
+
+;; saves all marathons that are in the global history to the specified director with optional widths and heights
+(defn save-all-marathons [dir & {:keys [width height] :or {width 700 height 420}}]
+ (let [charts (vals @marathons)]
+  (doseq [chart charts :let [title (get-chart-title chart)]]
+   (doseq [k ["fill" "dwell"]] (save-jfree-chart chart (str dir title k ".png") (keyword k) :width width :height height)))))
+ 
+ 
+
+
+
+;; dwells (->> (map (fn [[pane dwell fill]] dwell) charts)
+;;             (remove (fn [chart] (nil? (.getDataset (.getPlot chart))))));had to remove no deployment data charts
+;;fills (map (fn [[pane dwell fill]] fill) charts)
+;;(proc.util/set-bounds chart :x-axis :lower dxlow :upper dxhigh)
+;;(proc.util/set-bounds chart :y-axis :lower dylow :upper dyhigh)))
+;;(sync-chart-scales (concat dwells fills) :x-axis)
+;;(when syncys (do (sync-chart-scales dwells :y-axis) (sync-chart-scales fills :y-axis)))    
+;; (when phase-lines
+;;   (let [fills (map #(nth % 2) (vals @marathons))]
+;;     (doseq [fill fills] (add-phase-lines phstarts
+;;                                          (.getLowerBound (:y-axis (util/state fill)))
+;;                                          (.getUpperBound (:y-axis (util/state fill))) fill))))
+;; _ (doseq [fill fills] (Image.IO/write fill "jpg" "v:/"))
+;;(proc.util/sync-scales (concat dwells fills) :axis :x-axis)
+;;(when syncys (do
+;;               (proc.util/sync-scales dwells :axis :y-axis)
+;;               (proc.util/sync-scales fills :axis :y-axis)))
+;; _ (doseq [chart dwells] (do (proc.util/set-bounds chart :x-axis :lower dxlow :upper dxhigh)
+;;                             (proc.util/set-bounds chart :y-axis :lower dylow :upper dyhigh)))
+;; _ (doseq [chart fills] (do (proc.util/set-bounds chart :x-axis :lower fxlow :upper fxhigh)
+;;                            (proc.util/set-bounds chart :y-axis :lower fylow :upper fyhigh)))
+;; _ (proc.util/sync-scales (concat dwells fills) :axis :x-axis)   ;added for taa. want all charts same x
+;; _ (when syncys (do  ;it might be useful if our y-axes all match, too
+;;                  (proc.util/sync-scales dwells :axis :y-axis)
+;;                  (proc.util/sync-scales fills :axis :y-axis)))
+;; _  (doseq [dwell dwells] (add-trend-lines! dwell :bnds (:x (xy-bounds dwell)))) ;need to add trend lines after syncing charts
+
+;;  (when :save-fills 
+;;    (org.jfree.chart.ChartUtilities/saveChartAsPNG (File. (str title "-fill.png")) fill 800 800))
+;;  (when :save-dwells
+;;    (org.jfree.chart.ChartUtilities/saveChartAsPNG (File. (str title "-dwell.png")) dwell 800 800))
+;; (org.jfree.chart.ChartUtilities/saveChartAsPNG (File. "file.png" (last x) 100 100))
+
+
+
+
+
+
 ;;One function to make the files for the charts and display the charts at the same time
 (defn charts-from-unproc-run
   ([path]
