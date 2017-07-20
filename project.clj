637eff5e476f631158037c366d948522b2162133
joinr <vangelder@tantalus.net>
2017-07-19 21:22:35 -0500

    Bumped spork dep

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [spork "0.2.0.2-SNAPSHOT"]
+                 [spork "0.2.0.3-SNAPSHOT"]
                  [incanter "1.5.6"]
                  [iota "1.1.2"]]
   :repl-options {:init (do (println "Loading post processor")
