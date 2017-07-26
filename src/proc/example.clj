c547d7a1b213a01482041cb876812e4282805b7f
Mike Pavlak <michael.m.pavlak.civ@mail.mil>
2017-07-26 16:21:12 -0400

    added ppt to do charts

--- src/proc/example.clj
+++ src/proc/example.clj
@@ -62,7 +62,8 @@ Call with :fillbnds {:fxlow val0 :fxhigh val1 :fylow val2 :fyhigh val3} and/or
     (c/->lines charts phase-lines phstarts)
     (c/->save-dwell-fill root charts save-dwell save-fill)
     (c/->vis charts root interests vis)
-    (when ppt (c/charts->ppt [root] (:filename ppt) (:template ppt) (:num-per-slide ppt)))
+   (when ppt
+      (c/charts->ppt [root] (:filename ppt) (:template ppt) (:num-per-slide ppt)))
     (when return
       charts)))
 
