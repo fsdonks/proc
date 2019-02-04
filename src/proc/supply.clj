831990e8d25b5a1e9e9e059a14f9a54d3c449d82
tom <tom@that-place>
2019-02-04 01:19:15 -0600

    Add vba-round back in due to legacy code requirement.

--- src/proc/supply.clj
+++ src/proc/supply.clj
@@ -10,6 +10,15 @@
             [clojure.string :as str])
   (:use [proc.core]))
 
+;;generic, re-inserted due to poor merge.
+(defn vba-round
+  "rounds a number, n to 1 decimal place, but if the number is whole,
+  the .0 is dropped."
+  [n]
+  (let [x (str (round-to 1 n))
+        [o t] (clojure.string/split x #"\.")]
+    (if (= t "0") o x)))
+
 ;;Generic
 (defn units-locs-at
   "Given a time, t, and the root directory to the marathon run, this function will
